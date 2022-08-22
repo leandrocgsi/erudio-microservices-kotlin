4e598afe5feec1c3c8632b358b881658778fc938
leandrocgsi <leandrocgsi@gmail.com>
2022-08-22 20:38:15 -0300

    Creating and Connecting Microservices with Spring Cloud and Feign - Book Microservice

--- 08_CreatingandConnectingMicroserviceswithFeign/book-service/src/main/kotlin/br/com/erudio/controller/BookController.kt
+++ 08_CreatingandConnectingMicroserviceswithFeign/book-service/src/main/kotlin/br/com/erudio/controller/BookController.kt
@@ -1,11 +1,36 @@
 package br.com.erudio.controller
 
+import br.com.erudio.model.Book
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.Date
 
 @RestController
 @RequestMapping("book-service")
 class BookController {
 
+    @Autowired
+    private lateinit var environment: Environment
+
     //http://localhost:8100/book-service/1/BRL
+    @GetMapping(value = ["/{id}/{currency}"])
+    fun findBook(
+        @PathVariable("id") id: Long,
+        @PathVariable("currency") currency: String
+    ): Book? {
+        val port = environment.getProperty("local.server.port")
+        return Book(
+            id = 1L,
+            author = "Nigel Poulton",
+            title = "Docker Deep Dive",
+            launchDate = Date(),
+            price = 15.8.toDouble(),
+            currency = currency,
+            environment = port
+        )
+    }
 }
--- 08_CreatingandConnectingMicroserviceswithFeign/book-service/src/main/kotlin/br/com/erudio/model/Book.kt
+++ 08_CreatingandConnectingMicroserviceswithFeign/book-service/src/main/kotlin/br/com/erudio/model/Book.kt
@@ -1,4 +1,15 @@
 package br.com.erudio.model
 
-class Book {
-}
+import java.util.Currency
+import java.util.Date
+
+data class Book (
+
+    var id: Long = 0,
+    var author: String = "",
+    var launchDate: Date? = null,
+    var price: Double? = null,
+    var title: String = "",
+    var currency: String = "",
+    var environment: String? = ""
+)
