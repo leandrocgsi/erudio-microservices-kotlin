c2cdaddd2eca7cbe2bb4ec7b006dc92168464ce1
leandrocgsi <leandrocgsi@gmail.com>
2022-08-22 20:38:07 -0300

    Creating and Connecting Microservices with Spring Cloud and Feign - Book Microservice

---
+++ 08_CreatingandConnectingMicroserviceswithFeign/book-service/src/main/kotlin/br/com/erudio/controller/BookController.kt
@@ -0,0 +1,11 @@
+package br.com.erudio.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("book-service")
+class BookController {
+
+    //http://localhost:8100/book-service/1/BRL
+}
