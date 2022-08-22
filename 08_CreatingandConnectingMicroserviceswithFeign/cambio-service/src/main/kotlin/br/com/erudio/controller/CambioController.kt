6bb66882ca8c7c9884f6913858eaff16b921b1a3
leandrocgsi <leandrocgsi@gmail.com>
2022-08-22 16:14:11 -0300

    Creating and Connecting Microservices with Spring Cloud and Feign - Cambio Microservice

--- 08_CreatingandConnectingMicroserviceswithFeign/cambio-service/src/main/kotlin/br/com/erudio/controller/CambioController.kt
+++ 08_CreatingandConnectingMicroserviceswithFeign/cambio-service/src/main/kotlin/br/com/erudio/controller/CambioController.kt
@@ -1,6 +1,8 @@
 package br.com.erudio.controller
 
 import br.com.erudio.model.Cambio
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +13,9 @@ import java.math.BigDecimal
 @RequestMapping("cambio-service")
 class CambioController {
 
+    @Autowired
+    private lateinit var environment: Environment
+
     //http://localhost:8000/cambio-service/5/USD/BRL
     @GetMapping(value = ["/{amount}/{from}/{to}"])
     fun getCambio(
@@ -18,13 +23,16 @@ class CambioController {
         @PathVariable("from") from: String,
         @PathVariable("to") to: String
     ) : Cambio {
+
+        val port = environment.getProperty("local.server.port")
+
         return Cambio(
             id = 1L,
             from = from,
             to = to,
             conversionFactor = BigDecimal.ONE,
             convertedValue = BigDecimal.ONE,
-            environment = "PORT 8000"
+            environment = "PORT $port"
         )
     }
 }
