ab277df6f132ff75c33925d5bbd63be6137f078b
leandrocgsi <leandrocgsi@gmail.com>
2022-08-22 16:14:11 -0300

    Creating and Connecting Microservices with Spring Cloud and Feign - Cambio Microservice

--- 08_CreatingandConnectingMicroserviceswithFeign/cambio-service/src/main/kotlin/br/com/erudio/controller/CambioController.kt
+++ 08_CreatingandConnectingMicroserviceswithFeign/cambio-service/src/main/kotlin/br/com/erudio/controller/CambioController.kt
@@ -1,15 +1,26 @@
 package br.com.erudio.controller
 
 import br.com.erudio.model.Cambio
+import br.com.erudio.repository.CambioRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.RuntimeException
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 @RestController
 @RequestMapping("cambio-service")
 class CambioController {
+
+    @Autowired
+    private lateinit var environment: Environment
+
+    @Autowired
+    private lateinit var repository: CambioRepository
 
     //http://localhost:8000/cambio-service/5/USD/BRL
     @GetMapping(value = ["/{amount}/{from}/{to}"])
@@ -18,13 +29,16 @@ class CambioController {
         @PathVariable("from") from: String,
         @PathVariable("to") to: String
     ) : Cambio {
-        return Cambio(
-            id = 1L,
-            from = from,
-            to = to,
-            conversionFactor = BigDecimal.ONE,
-            convertedValue = BigDecimal.ONE,
-            environment = "PORT 8000"
-        )
+
+        val cambio = repository.findByFromAndTo(from, to) ?: throw RuntimeException("Currency Unsupported")
+        val port = environment.getProperty("local.server.port")
+
+        val conversionFactor = cambio.conversionFactor
+        val convertedValue = conversionFactor.multiply(amount)
+
+        cambio.convertedValue = convertedValue.setScale(2, RoundingMode.CEILING)
+        cambio.environment = port
+
+        return cambio
     }
 }
