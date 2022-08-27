7cb93fcfef3a9a77a2908a86880fe2513f61211d
leandrocgsi <leandrocgsi@gmail.com>
2022-08-27 13:44:32 -0300

    Load Balancing with Zookeeper, Feign and Spring Cloud LoadBalancer - Adding API Gateway

--- 10_LoadBalancingwithZookeeperFeignandSCLoadBalancer/api-gateway/src/main/kotlin/br/com/erudio/configuration/ApiGatewayConfiguration.kt
+++ 10_LoadBalancingwithZookeeperFeignandSCLoadBalancer/api-gateway/src/main/kotlin/br/com/erudio/configuration/ApiGatewayConfiguration.kt
@@ -1,4 +1,26 @@
 package br.com.erudio.configuration
 
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec
+import org.springframework.cloud.gateway.route.builder.PredicateSpec
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class ApiGatewayConfiguration {
+
+    @Bean
+    fun gatewayRouter(builder: RouteLocatorBuilder): RouteLocator {
+        return builder.routes()
+            .route {p: PredicateSpec ->
+                p.path("/get")
+                    .filters{f: GatewayFilterSpec ->
+                        f.addRequestHeader("Hello", "Word")
+                        f.addRequestParameter("Hello", "Word")
+                    }
+                    .uri("http://httpbin.org:80")
+            }
+            .build()
+    }
 }
